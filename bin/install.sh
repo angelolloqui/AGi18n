4fcddc8243159e72bc792251d86f0a3a5950708e
Angel G. Olloqui <angelgarcia.mail@gmail.com>
2013-03-18 11:27:16 +0100

    Update install.sh
    Rehash added to install script

--- bin/install.sh
+++ bin/install.sh
@@ -8,4 +8,7 @@ echo "Downloading agi18n..."
 curl https://raw.github.com/angelolloqui/AGi18n/master/bin/agi18n > /usr/local/bin/agi18n
 chmod +x /usr/local/bin/agi18n
 
-echo "Downloads complete. Close your terminal and try using genxibstrings or agi18n commands in your xcode project source folder"
+echo "Rehashing..."
+rehash
+
+echo "Downloads complete. Try using genxibstrings or agi18n commands in your xcode project source folder"
