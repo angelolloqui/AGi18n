023cf83ad5403256e89b1055473be000874f566d
Angel Garcia <angelgarcia.mail@gmail.com>
2013-06-09 20:09:46 +0200

    Added ruby and command line tools checks in install process

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+hash genstrings &> /dev/null
+if [ $? -eq 1 ]; then
+    echo >&2 "Xcode Command Line Tools not found. Please install them first by going to XCode -> Preferences -> Downloads and try again"
+    exit 1
+fi
+
+RUBY=( `ruby --version` )
+if [[ ${RUBY[1]} == *"1.8"* ]]; then 
+	echo >&2 "WARNING: your ruby version installed is ${RUBY[1]} but a minimum of 1.9 is required for this tool. Please, update your ruby environment before executing agi18n commands"
+fi
+
 echo "Downloading genxibstrings..."
 curl https://raw.github.com/angelolloqui/AGi18n/master/bin/genxibstrings > /usr/local/bin/genxibstrings
 chmod +x /usr/local/bin/genxibstrings
