a958c943ae37c3d01d17f2002602fce79287bdd0
Takeru Chuganji <takeru@hoppenichu.com>
2016-09-12 14:23:38 +0900

    Set title only when differed from default one

--- lib/UIButton+AGi18n.m
+++ lib/UIButton+AGi18n.m
@@ -11,13 +11,22 @@
 @implementation UIButton (AGi18n)
 
 - (void)localizeFromNib {
-
-    //Replace text with localizable version
-    NSArray *states = @[@(UIControlStateNormal), @(UIControlStateHighlighted), @(UIControlStateDisabled), @(UIControlStateSelected), @(UIControlStateApplication)];
-    for (NSNumber *state in states) {
-        NSString *title = [self titleForState:state.integerValue];
+    // Replace text with localizable version
+    void (^localizeForState)(UIControlState) = ^(UIControlState state) {
+        NSString *title = [self titleForState:state];
         if (title.length > 0) {
-            [self setTitle:[[NSBundle mainBundle] localizedStringForKey:title value:@"" table:nil] forState:state.integerValue];
+            [self setTitle:[[NSBundle mainBundle] localizedStringForKey:title value:@"" table:nil] forState:state];
+        }
+    };
+    // Force replace for normal state
+    localizeForState(UIControlStateNormal);
+
+    NSString *normalTitle = [self titleForState:UIControlStateNormal];
+    NSArray *otherStates = @[@(UIControlStateHighlighted), @(UIControlStateDisabled), @(UIControlStateSelected), @(UIControlStateApplication)];
+    for (NSNumber *state in otherStates) {
+        NSString *title = [self titleForState:state.unsignedIntegerValue];
+        if (title.length > 0 && ![title isEqualToString:normalTitle]) {
+            localizeForState(state.unsignedIntegerValue);
         }
     }
 }
