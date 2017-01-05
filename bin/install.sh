1d0b7e1e6e0c0dd15ea558b14f005906fafb661a
Angel G. Olloqui <angelgarcia.mail@gmail.com>
2017-01-05 10:05:59 +0100

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -12,11 +12,11 @@ if [[ ${RUBY[1]} == *"1.8"* ]]; then
 fi
 
 echo "Downloading genxibstrings..."
-curl https://raw.github.com/angelolloqui/AGi18n/master/bin/genxibstrings > /usr/local/bin/genxibstrings
+curl -L https://raw.github.com/angelolloqui/AGi18n/master/bin/genxibstrings > /usr/local/bin/genxibstrings
 chmod +x /usr/local/bin/genxibstrings
 
 echo "Downloading agi18n..."
-curl https://raw.github.com/angelolloqui/AGi18n/master/bin/agi18n > /usr/local/bin/agi18n
+curl -L https://raw.github.com/angelolloqui/AGi18n/master/bin/agi18n > /usr/local/bin/agi18n
 chmod +x /usr/local/bin/agi18n
 
 echo "Rehashing..."
